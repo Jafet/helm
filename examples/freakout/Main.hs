7dd74dbf0bf9afdaaa04f6ab13a728d3428a95d6
Jafet <jafet.vixle@gmail.com>
2017-11-17 17:20:38 +0800

    freakout: basic simulation and buggy physics

--- examples/freakout/Main.hs
+++ examples/freakout/Main.hs
@@ -7,7 +7,7 @@ module Main where
 
 import qualified Data.List as L
 import qualified Data.Map as M
-import           Data.Maybe (listToMaybe, catMaybes)
+import           Data.Maybe (fromMaybe, listToMaybe, catMaybes)
 import           Debug.Trace (traceShow)
 import           Text.Printf (printf)
 
@@ -27,7 +27,7 @@ import qualified Helm.Keyboard as Keyboard
 import qualified Helm.Mouse as Mouse
 import qualified Helm.Sub as Sub
 import qualified Helm.Time as Time
-import           Helm.Time (Time, second)
+import           Helm.Time (Time, inSeconds, second)
 
 import qualified Paths_helm as Paths
 
@@ -39,6 +39,7 @@ data Action
   | Simulate Double             -- ^ Physics step.
   | PaddleTarget (V2 Double)    -- ^ Change where the paddle moves towards.
   | Pause                       -- ^ Pause or resume the game.
+  | NextLevel                   -- ^ Internal action that advances to the next level.
 
 -- | Represents the active playing field.
 -- Note that all Field coordinates are relative to the game's gridSize.
@@ -64,11 +65,14 @@ paddleSize = V2 4 1
 gridSize :: V2 Int
 gridSize = V2 48 32
 
+initBall :: Ball
+initBall = Ball {ballPos = V2 12 20, ballVel = V2 10 10}
+
 initField :: [Brick] -> Field
 initField bricks = Field
   { activeBricks = bricks
-  , activeBalls  = [Ball {ballPos = V2 14 20, ballVel = V2 5 5}]
-  , paddlePos    = V2 24 30
+  , activeBalls  = [initBall]
+  , paddlePos    = V2 24 28
   }
 
 initGame :: GameState
@@ -91,11 +95,65 @@ update state@GameState { .. } Pause
   | not (isGameOver state) = (state { isRunning = not isRunning }, Cmd.none)
 
 update state@GameState { .. } (PaddleTarget target)
-  = (state { paddleTarget = Just target }, Cmd.none)
+  = (state { paddleTarget = Just (target / pure pixelsPerGrid) }, Cmd.none)
 
 update state@GameState { .. } (Simulate dt)
-  | not (isGameOver state) = (state, Cmd.none)
-  where handleCollisions [] movedBalls bricks score = undefined
+  | not (isGameOver state) && isRunning =
+      ( state
+        { activeField  = field''
+        , currentScore = score'
+        , livesLeft    = livesLeft'
+        }
+      , Cmd.none
+      )
+  where
+        (field', score') =
+          environmentCollisions
+            (filter (not . ballDropped) $ activeBalls activeField) []
+            (activeBricks activeField)
+            currentScore
+
+        (livesLeft', field'')
+          | null (activeBalls field') && livesLeft > 0 =
+              ( livesLeft - 1
+              , field' { activeBalls = [initBall] }
+              )
+          | otherwise = (livesLeft, field')
+
+        -- We collide each ball with all other objects in sequence.
+        -- This takes care of balls hitting corner interiors:
+        -- the ball simply bounces off both walls.
+        stepBalls = map (\ball@Ball { .. } ->
+                           ball { ballPos = ballPos + ballVel * pure (inSeconds dt) })
+
+        -- Ball collisions with bricks, paddle and walls.
+        environmentCollisions [] movedBalls bricks score =
+          (Field
+            { activeBricks = bricks
+            , activeBalls  = stepBalls movedBalls
+            , paddlePos    = paddlePos'
+            }
+          , score
+          )
+        environmentCollisions (ball : balls) movedBalls bricks score =
+          let ball2 = fromMaybe ball (collideBallField ball)
+              ball3 = fromMaybe ball2 (collideBallRect ball paddleRect')
+              (ball4, bricks', score') = collideBricks ball3 bricks [] score
+          in environmentCollisions balls (ball4:movedBalls) bricks' score'
+
+        collideBricks ball [] doneBricks score = (ball, doneBricks, score)
+        collideBricks ball (brick@Brick { .. } : bricks) doneBricks score =
+          case collideBallRect ball (brickTopLeft, brickBottomRight) of
+            Nothing -> collideBricks ball bricks (brick:doneBricks) score
+            Just ball' -> collideBricks ball' bricks doneBricks (score + brickValue)
+
+        V2 paddleWidth _ = paddleSize
+        V2 fieldWidth _ = fmap fromIntegral gridSize
+        clamp low high = min high . max low
+        paddlePos' = let V2 x y = paddlePos activeField
+                         V2 x' _ = fromMaybe (paddlePos activeField) paddleTarget
+                     in V2 (clamp (paddleWidth/2) (fieldWidth - paddleWidth/2) x') y
+        paddleRect' = (paddlePos' - paddleSize/2, paddlePos' + paddleSize/2)
 
 update state _ = (state, Cmd.none)
 
@@ -113,6 +171,7 @@ data Ball = Ball
   { ballPos :: !(V2 Double)
   , ballVel :: !(V2 Double)
   }
+  deriving Show
 
 ballRadius :: Double
 ballRadius = 0.5
@@ -128,28 +187,33 @@ collideBallBall
   | otherwise =
       -- Reflect the balls off their mutual tangent plane.
       -- Unrealistic, but simple and generally looks OK.
-      Just ( ballA { ballVel = reflect tangent velA }
-           , ballB { ballVel = reflect tangent velB }
+      Just ( ballA { ballVel = reflectAway (posB - posA) velA }
+           , ballB { ballVel = reflectAway (posA - posB) velB }
            )
-  where tangent = signorm $ V2 (yA - yB) (xA - xB)
 
 -- | Collide a ball with a rectangle, given by its top-left and bottom-right corners.
 collideBallRect :: Ball -> (V2 Double, V2 Double) -> Maybe Ball
 collideBallRect ball@Ball{ ballPos = ballPos@(V2 x y), ballVel = ballVel }
-                (V2 rectLeft rectTop, V2 rectBottom rectRight) =
+                rect@(V2 rectLeft rectTop, V2 rectRight rectBottom) =
   -- Check corners
   case filter (\corner -> quadrance (ballPos - corner) <= ballRadius^2) corners of
     (corner@(V2 xCorner yCorner):_) ->
       -- Bounce off the corner
-      let tangent = signorm $ V2 (y - yCorner) (x - xCorner)
-      in Just $ ball { ballVel = reflect tangent ballVel }
+      traceShow ("corner", rect, ball) $
+      Just $ ball { ballVel = reflectAway (ballPos - corner) ballVel }
     [] -- Check edges
       | rectLeft <= x && x <= rectRight &&
-        y + ballRadius >= rectBottom && y - ballRadius >= rectTop ->
-          Just $ ball { ballVel = reflect (V2 1 0) ballVel }
-      | rectBottom <= y && y <= rectTop &&
-        x + ballRadius >= rectLeft && x - ballRadius >= rectRight ->
-          Just $ ball { ballVel = reflect (V2 0 1) ballVel }
+        y - ballRadius <= rectBottom && y + ballRadius >= rectTop ->
+          let normal | abs (y - rectTop) < abs (y - rectBottom) = V2 0 (-1)
+                     | otherwise                                = V2 0 1
+          in traceShow ("horz edge", rect, ball) $
+             Just $ ball { ballVel = reflectAway normal ballVel }
+      | rectTop <= y && y <= rectBottom &&
+        x + ballRadius >= rectLeft && x - ballRadius <= rectRight ->
+          let normal | abs (x - rectLeft) < abs (y - rectRight) = V2 (-1) 0
+                     | otherwise                                = V2 1 0
+          in traceShow ("vert edge", rect, ball) $
+             Just $ ball { ballVel = reflectAway normal ballVel }
       | otherwise ->
         -- no collision
           Nothing
@@ -159,7 +223,7 @@ collideBallRect ball@Ball{ ballPos = ballPos@(V2 x y), ballVel = ballVel }
 collideBallField :: Ball -> Maybe Ball
 collideBallField ball =
   listToMaybe . catMaybes $ map (collideBallRect ball)
-    [ (V2 (-100) (gridHeight-100), V2 (gridWidth+100) gridHeight)
+    [ (V2 (-100) (-100), V2 (gridWidth+100) 0)
     , (V2 (-100) (-100), V2 0 (gridHeight+100))
     , (V2 gridWidth (-100), V2 gridWidth (gridHeight+100))
     ]
@@ -172,10 +236,19 @@ ballDropped Ball { ballPos = V2 x y } = y + ballRadius > gridHeight
 
 -- | Reflect a vector. Used to bounce objects after collisions.
 reflect :: V2 Double -> V2 Double -> V2 Double
-reflect tangent -- ^ Tangent vector
-        ray     -- ^ Vector to reflect
-  = ray - pure (2 * dot ray tangent) * tangent
+reflect normal -- ^ Vector normal to the reflection plane (line)
+        ray    -- ^ Vector to reflect
+  = ray - pure (2 * dot ray unitNorm) * unitNorm
+  where unitNorm = signorm normal
 
+-- | As 'reflect', but only reflect if the ray is opposite to the normal.
+--   This means that objects already leaving a collision will not be affected.
+reflectAway :: V2 Double -> V2 Double -> V2 Double
+reflectAway normal ray
+  | project >= 0 = ray
+  | otherwise    = ray - pure (2 * project) * unitNorm
+  where unitNorm = signorm normal
+        project  = dot ray unitNorm
 
 brick1, brick2, brick3, brick4 :: Brick
 brick1 = Brick
@@ -204,6 +277,7 @@ brick4 = Brick
   }
 
 -- | Level 1 is a simple rectangular wall, with higher-value bricks on top.
+-- Score: 2000
 level1 :: [Brick]
 level1 =
   [ placeBrick (V2 x y) brick4
@@ -213,10 +287,10 @@ level1 =
   | y <- [5], x <- brickColumns
   ] ++
   [ placeBrick (V2 x y) brick2
-  | y <- [6, 7], x <- brickColumns
+  | y <- [6], x <- brickColumns
   ] ++
   [ placeBrick (V2 x y) brick1
-  | y <- [8 .. 10], x <- brickColumns
+  | y <- [7 .. 9], x <- brickColumns
   ]
   where placeBrick pos brick@Brick{..} =
           brick { brickTopLeft = pos
@@ -229,9 +303,10 @@ level1 =
 subscriptions :: Sub SDLEngine Action
 subscriptions = Sub.batch
   [ Mouse.clicks $ \_ _ -> Pause
+  , Mouse.moves $ PaddleTarget . fmap fromIntegral
   , Keyboard.presses $ \key -> (case key of
-      _                 -> DoNothing)
-  , Time.fps 60 Simulate
+      _ -> DoNothing)
+  , Time.fps 300 Simulate
   ]
 
 viewBrick :: Brick -> Form SDLEngine
@@ -259,26 +334,50 @@ viewField Field { .. } = group $
 view :: GameState -> Graphics SDLEngine
 view state@GameState { .. } =
   Graphics2D $ collage $
+    [ statsOverlay ] ++
     map (scale pixelsPerGrid)
-        [ viewField activeField, pausedOverlay ]
-  where pixelsPerGrid = min scaleX scaleY
-        fullRect = fmap fromIntegral gridSize
-        V2 gridWidth   gridHeight   = fullRect
+        [ viewField activeField
+        , pausedOverlay
+        , gameOverOverlay
+        ]
+  where fieldRect@(V2 fieldWidth fieldHeight) = fmap fromIntegral gridSize
         V2 windowWidth windowHeight = fmap fromIntegral windowDims
-        scaleX = windowWidth  / gridWidth
-        scaleY = windowHeight / gridHeight
+
+        statsHeight = 16
+        statsOverlay = group $
+          [ move (V2 (windowWidth/2) (windowHeight - statsHeight - 10)) $
+            text $ Text.color (rgb 1.0 1.0 1.0) $ Text.height statsHeight $
+            Text.toText $ printf "Score: %d   Lives: %d" currentScore livesLeft
+          ]
 
         pausedOverlay
           | isRunning = blank
           | otherwise = group $
-            [ move (fullRect / 2) $ filled (rgba 0.5 0.5 0.5 0.3) $ rect fullRect
-            , toForm $ center (fullRect / 2) $ collage $
+            [ move (fieldRect / 2) $ filled (rgba 0.5 0.5 0.5 0.3) $ rect fieldRect
+            , toForm $ center (fieldRect / 2) $ collage $
               [ text $ Text.color (rgb 0.3 0.3 0.3) $ Text.height 4 $
                 Text.toText "PAUSED" ]
             ]
 
+        gameOverOverlay
+          | not (isGameOver state) = blank
+          | otherwise = group $
+            [ move (fieldRect / 2) $ filled (rgba 0.5 0.5 0.5 0.3) $ rect fieldRect
+            , toForm $ center (fieldRect / 2) $ collage $
+              [ text $ Text.color (rgb 1.0 0.2 0.0) $ Text.height 4 $
+                Text.toText "GAME OVER" ]
+            ]
+
 windowDims :: V2 Int
 windowDims = V2 768 512
+
+pixelsPerGrid :: Double
+pixelsPerGrid = min scaleX scaleY
+  where fieldRect = fmap fromIntegral gridSize
+        V2 gridWidth   gridHeight   = fieldRect
+        V2 windowWidth windowHeight = fmap fromIntegral windowDims
+        scaleX = windowWidth  / gridWidth
+        scaleY = windowHeight / gridHeight
 
 main :: IO ()
 main = do
